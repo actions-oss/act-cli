e736468cde29822a1bc1bc8abc2f6d0e69923885
ChristopherHX <christopher.homberger@web.de>
2025-08-28 20:10:29 +0200

    Refactor resolveAliases function for simplicity

--- pkg/model/anchors.go
+++ pkg/model/anchors.go
@@ -4,7 +4,7 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-func resolveAliases(node *yaml.Node, anchors map[string]*yaml.Node, path map[*yaml.Node]bool) error {
+func resolveAliasesExt(node *yaml.Node, path map[*yaml.Node]bool) error {
     switch node.Kind {
     case yaml.AliasNode:
         aliasTarget := node.Alias
@@ -16,17 +16,21 @@ func resolveAliases(node *yaml.Node, anchors map[string]*yaml.Node, path map[*ya
         }
         path[aliasTarget] = true
         *node = *aliasTarget
-        if err := resolveAliases(node, anchors, path); err != nil {
+        if err := resolveAliases(node, path); err != nil {
             return err
         }
         delete(path, aliasTarget)
 
     case yaml.MappingNode, yaml.SequenceNode:
         for _, child := range node.Content {
-            if err := resolveAliases(child, anchors, path); err != nil {
+            if err := resolveAliases(child, path); err != nil {
                 return err
             }
         }
     }
     return nil
+}
+
+func resolveAliases(node *yaml.Node) error {
+    return resolveAliasesExt(node, map[*yaml.Node]bool{})
 }
