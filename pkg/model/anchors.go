22526cca7135093c48a188a3a90e2be96c95928a
ChristopherHX <christopher.homberger@web.de>
2025-08-28 20:19:56 +0200

    fix space

--- pkg/model/anchors.go
+++ pkg/model/anchors.go
@@ -7,32 +7,32 @@ import (
 )
 
 func resolveAliasesExt(node *yaml.Node, path map[*yaml.Node]bool) error {
-    switch node.Kind {
-    case yaml.AliasNode:
-        aliasTarget := node.Alias
-        if aliasTarget == nil {
-            return errors.New("unresolved alias node")
-        }
-        if path[aliasTarget] {
-            return errors.New("regression detected: circular alias")
-        }
-        path[aliasTarget] = true
-        *node = *aliasTarget
-        if err := resolveAliasesExt(node, path); err != nil {
-            return err
-        }
-        delete(path, aliasTarget)
+	switch node.Kind {
+	case yaml.AliasNode:
+		aliasTarget := node.Alias
+		if aliasTarget == nil {
+			return errors.New("unresolved alias node")
+		}
+		if path[aliasTarget] {
+			return errors.New("regression detected: circular alias")
+		}
+		path[aliasTarget] = true
+		*node = *aliasTarget
+		if err := resolveAliasesExt(node, path); err != nil {
+			return err
+		}
+		delete(path, aliasTarget)
 
-    case yaml.MappingNode, yaml.SequenceNode:
-        for _, child := range node.Content {
-            if err := resolveAliasesExt(child, path); err != nil {
-                return err
-            }
-        }
-    }
-    return nil
+	case yaml.MappingNode, yaml.SequenceNode:
+		for _, child := range node.Content {
+			if err := resolveAliasesExt(child, path); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
 
 func resolveAliases(node *yaml.Node) error {
-    return resolveAliasesExt(node, map[*yaml.Node]bool{})
+	return resolveAliasesExt(node, map[*yaml.Node]bool{})
 }
