be974ddc85074d34f03efbdc3b884578185ad273
Christopher Homberger <christopher.homberger@web.de>
2025-10-01 01:14:20 +0200

    fix lint

--- pkg/schema/gitea_schema_test.go
+++ pkg/schema/gitea_schema_test.go
@@ -14,6 +14,6 @@ func TestGiteaSchemaFactory(t *testing.T) {
 
 	data, err := json.MarshalIndent(schema, "", "  ")
 	assert.NoError(t, err)
-	err = os.WriteFile("gitea_workflow_schema.json", data, 0o600)
+	err = os.WriteFile("gitea_workflow_schema.json", append(data, "\n"...), 0o600)
 	assert.NoError(t, err)
 }
